67c5aa6ab4364014606f44adaf1a25b9fe60e64d
dale4id <dale4id@yahoo.com>
2021-01-17 22:38:41 +0700

    Updated by CrossUI No-Code App Builder

--- App/js/Project.js
+++ App/js/Project.js
@@ -4,9 +4,199 @@ xui.Class('App.Project', 'xui.Module',{
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
-            append((new xui.UI.Pane())
-            .setHost(host,"ctl_pane10")
-            .setDock("fill")
+            append(
+                xui.create("xui.UI.Panel")
+                .setHost(host,"xui_ui_panel4")
+                .setLeft("9.904761904761905em")
+                .setTop("25.142857142857142em")
+                .setCaption("Project")
+            );
+            
+            host.xui_ui_panel4.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div19")
+                .setDock("fill")
+                .setLeft("1.5238095238095237em")
+                .setTop("2.2857142857142856em")
+                .setWidth("35.12380952380953em")
+                .setHeight("20.64761904761905em")
+            );
+            
+            host.xui_ui_div19.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div20")
+                .setDock("top")
+                .setHeight("1.1428571428571428em")
+            );
+            
+            host.xui_ui_div19.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div31")
+                .setDock("top")
+                .setDockMargin({
+                    "left":10,
+                    "top":0,
+                    "right":10,
+                    "bottom":0
+                })
+                .setLeft("11.428571428571429em")
+                .setTop("17.523809523809526em")
+                .setHeight("4.495238095238095em")
+            );
+            
+            host.xui_ui_div31.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div32")
+                .setDock("left")
+                .setLeft("5.333333333333333em")
+                .setTop("6.857142857142857em")
+                .setWidth("20.19047619047619em")
+            );
+            
+            host.xui_ui_div32.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input9")
+                .setDirtyMark(false)
+                .setDock("top")
+                .setDockMargin({
+                    "left":5,
+                    "top":0,
+                    "right":5,
+                    "bottom":0
+                })
+                .setLeft("0.7619047619047619em")
+                .setTop("5.333333333333333em")
+                .setHeight("4em")
+                .setLabelSize("2em")
+                .setLabelPos("top")
+                .setLabelCaption("Project Name")
+                .setLabelHAlign("left")
+            );
+            
+            host.xui_ui_div31.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div33")
+                .setDock("left")
+                .setLeft("17.523809523809526em")
+                .setTop("3.0476190476190474em")
+            );
+            
+            host.xui_ui_div33.append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput132")
+                .setDirtyMark(false)
+                .setDock("top")
+                .setDockMargin({
+                    "left":5,
+                    "top":0,
+                    "right":5,
+                    "bottom":0
+                })
+                .setLeft("3.0476190476190474em")
+                .setTop("1.5238095238095237em")
+                .setHeight("4em")
+                .setLabelSize("2em")
+                .setLabelPos("top")
+                .setLabelCaption("Status")
+                .setLabelHAlign("left")
+                .setItems([
+                    {
+                        "id":"a",
+                        "caption":"item 1",
+                        "imageClass":"xui-icon-number1"
+                    },
+                    {
+                        "id":"b",
+                        "caption":"item 2",
+                        "imageClass":"xui-icon-number2"
+                    },
+                    {
+                        "id":"c",
+                        "caption":"item 3",
+                        "imageClass":"xui-icon-number3"
+                    },
+                    {
+                        "id":"d",
+                        "caption":"item 4",
+                        "imageClass":"xui-icon-number4",
+                        "disabled":true
+                    }
+                ])
+            );
+            
+            host.xui_ui_div31.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div77")
+                .setDock("left")
+                .setLeft("26.666666666666668em")
+                .setTop("3.0476190476190474em")
+                .setWidth("11.504761904761905em")
+            );
+            
+            host.xui_ui_div77.append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput147")
+                .setDirtyMark(false)
+                .setLeft("3.8095238095238093em")
+                .setTop("2.2857142857142856em")
+                .setHeight("4em")
+                .setLabelSize("2em")
+                .setLabelPos("top")
+                .setLabelCaption("Start Date")
+                .setLabelHAlign("left")
+                .setType("date")
+                .setDock("top")
+                .setDockMargin({
+                    "left":5,
+                    "top":0,
+                    "right":5,
+                    "bottom":0
+                })
+            );
+            
+            host.xui_ui_div19.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div34")
+                .setDock("top")
+                .setDockMargin({
+                    "left":10,
+                    "top":0,
+                    "right":10,
+                    "bottom":0
+                })
+                .setLeft("11.428571428571429em")
+                .setTop("17.523809523809526em")
+                .setHeight("12.952380952380953em")
+            );
+            
+            host.xui_ui_div34.append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div60")
+                .setDock("left")
+                .setLeft("8.380952380952381em")
+                .setTop("4.571428571428571em")
+                .setWidth("41.98095238095238em")
+            );
+            
+            host.xui_ui_div60.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input18")
+                .setDirtyMark(false)
+                .setDock("fill")
+                .setDockMargin({
+                    "left":5,
+                    "top":0,
+                    "right":5,
+                    "bottom":0
+                })
+                .setLeft("7.619047619047619em")
+                .setTop("6.857142857142857em")
+                .setHeight("4em")
+                .setLabelSize("2em")
+                .setLabelPos("top")
+                .setLabelCaption("Keterangan")
+                .setLabelHAlign("left")
+                .setMultiLines(true)
             );
             
             return children;
